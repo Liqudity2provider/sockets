f7f30330c7ab5f71e94515c6ee4e99fcb4a814c3
oleksandr_hrechenko <oleksandr_hrechenko@epam.com>
2022-06-10 14:48:42 +0200

    Refactored logger_manager.py to get simple workflow to get loggers in any module

--- logger_manager.py
+++ logger_manager.py
@@ -2,24 +2,26 @@ import logging
 
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 
+logging.basicConfig(level=logging.DEBUG)
 
-def get_logger(log_name, level=logging.DEBUG, file=False):
+
+def get_logger(log_name,
+               level=logging.DEBUG,
+               file=False,
+               file_level=logging.ERROR):
     """
+    :param file_level: NOTSET, DEBUG, INFO, WARNING, ERROR
     :param file: bool
     :param level: NOTSET, DEBUG, INFO, WARNING, ERROR
     :type log_name: str
     :type level: int
     """
 
-    if file:
-        logging.basicConfig(filename=f"{log_name}_logfile.log",
-                            filemode="a",
-                            format=LOG_FORMAT,
-                            level=logging.ERROR)
-    else:
-        logging.basicConfig()
+    _logger = logging.getLogger(log_name)
 
-    module_logger = logging.getLogger(log_name)
-    if level:
-        module_logger.setLevel(level)
-    return module_logger
+    if file:
+        fh = logging.FileHandler(f"monitor_logfile.log")
+        fh.setLevel(file_level)
+        _logger.addHandler(fh)
+
+    return _logger
